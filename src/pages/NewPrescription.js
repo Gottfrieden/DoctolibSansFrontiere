3481e532332f2b0ffc9cfe901a26061ab7382039
Vianney Bouault <vbouault@hotmail.fr>
2020-06-24 19:08:09 +0200

    fix indentation

--- src/pages/NewPrescription.js
+++ src/pages/NewPrescription.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Newprescription = () => {
-    return (
-        <>
-            <h2>New Prescription</h2>
-            <main id='newprescription-main-container' className='main-container'>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <h2>New Prescription</h2>
+        <main id='newprescription-main-container' className='main-container'>
+        </main>
+    </>
+  )
 }
 
 export default Newprescription;
