382b5c1c6f68c1e599f5ce56b5cb8e022cf06b80
Vianney Bouault <vbouault@hotmail.fr>
2020-06-24 18:23:47 +0200

    connection firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import '../styles/App.css';
+import React, { useEffect, useState } from 'react';
+import * as firebase from 'firebase';
+import configFirebase from './firebase';
 
 function App() {
+
+  const [state, setstate] = useState()
+
+  useEffect(() => {
+    firebase.initializeApp(configFirebase);
+    const ref = firebase.database().ref('0');
+    ref.on('value', snapshot => {
+      console.log(snapshot.val())
+      setstate(snapshot.val())
+    })
+  }, [])
+
+
   return (
     <div className="App">
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkWIgJeGqc5unvAPHfXNXjPOlQti5-aUE",
+  authDomain: "hackathon-dev2020.firebaseapp.com",
+  databaseURL: "https://hackathon-dev2020.firebaseio.com",
+  projectId: "hackathon-dev2020",
+  storageBucket: "hackathon-dev2020.appspot.com",
+  messagingSenderId: "739471949",
+  appId: "1:739471949:web:197b7499d9cbf1d7eca204",
+  measurementId: "G-EKPJZCQMTP"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
