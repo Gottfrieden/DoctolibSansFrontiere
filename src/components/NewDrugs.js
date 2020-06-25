e1231e60ccd69597096aaf194131a919c15b70ec
Vianney Bouault <vbouault@hotmail.fr>
2020-06-25 13:32:25 +0200

    changement tables firebase

--- src/components/NewDrugs.js
+++ src/components/NewDrugs.js
@@ -1,37 +1,36 @@
 import React from 'react'
 
-const NewDrugs = ({drug, index, handleChange}) => {
+const NewDrugs = ({drug, index, handleChange, handleChangeCheckBox}) => {
   return (
-    <>
+    <div className='NewPrescription-drug-container'>
         <input type='text' name='name' placeholder='name' onChange={e => handleChange(e, index)} value={drug.name}/>
 
-        <label htmlFor="quantity">quantity</label>
-        <input type='number' name='quantity' min='0' onChange={e => handleChange(e, index)} value={drug.quantity}/>
+        <div className='newPrescription-ligne'>
+          <label htmlFor="quantity">quantity</label>
+          <input type='number' name='quantity' min='0' onChange={e => handleChange(e, index)} value={drug.quantity}/>
+        </div>
 
-        <label htmlFor="frequency">frequency</label>
-        <input type='number' name='frequency' min='0' onChange={e => handleChange(e, index)} value={drug.frequency}/>
+        <div className='newPrescription-ligne'>
+          <label htmlFor="frequency">frequency</label>
+          <input type='number' name='frequency' min='0' onChange={e => handleChange(e, index)} value={drug.frequency}/>
+        </div>
 
-        <input type="checkbox" id="breakfast" onChange={e => handleChange(e, index)} value={drug.breakfast}/>
-        <label htmlFor="breakfast">breakfast</label>
+        <div className='newPrescription-ligne'>
+          <input type="checkbox" name="breakfast" onChange={e => handleChangeCheckBox(e, index)} value={drug.breakfast}/>
+          <label htmlFor="breakfast">breakfast</label>
 
-        <input type="checkbox" id="lunch" onChange={e => handleChange(e, index)} value={drug.lunch}/>
-        <label htmlFor="lunch">lunch</label>
+          <input type="checkbox" name="lunch" onChange={e => handleChangeCheckBox(e, index)} value={drug.lunch}/>
+          <label htmlFor="lunch">lunch</label>
 
-        <input type="checkbox" id="diner" onChange={e => handleChange(e, index)} value={drug.diner}/>
-        <label htmlFor="diner">diner</label>
+          <input type="checkbox" name="diner" onChange={e => handleChangeCheckBox(e, index)} value={drug.diner}/>
+          <label htmlFor="diner">diner</label>
+        </div>
 
         <input type='number' name='traitment_duration' min='0' onChange={e => handleChange(e, index)} value={drug.traitment_duration}/>
 
         <textarea name='additional_information' placeholder='additional information' onChange={e => handleChange(e, index)} value={drug.additional_information}/>
-    </>
+    </div>
   )
 }
 
 export default NewDrugs
-
-/* quantity : 0,
-frequency : 1,
-breakfast :false,
-lunch : false,
-diner : false,
-traitment_duration : 0, */
