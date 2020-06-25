d0d7425874c23952daa525c8034b6588f8a69825
Geoffroy <geoffroy.crabieres@gmail.com>
2020-06-25 17:10:56 +0200

    added key

--- src/components/SingleDrug.js
+++ src/components/SingleDrug.js
@@ -8,25 +8,25 @@ export default function SingleDrug (props) {
     <>
     {props.singlePrescription !== undefined && props.singlePrescription.drugs.map(d => {
           return (
-            <div className='single-drug-container content-container'>
-          <div className='drug-name'>
-            <h3>{d.name}</h3>
-          </div>
-          <div className='single-drug-content-container'>
-            <div className='image-content-container'>
-              <span className='time-image' />
-              <p>{d.quantity} {d.quantity > 1 ? 'pills' : 'pill'} {d.frequency}x per days</p>
+            <div key={d.name} className='single-drug-container content-container'>
+              <div className='drug-name'>
+                <h3>{d.name}</h3>
+              </div>
+              <div className='single-drug-content-container'>
+                <div className='image-content-container'>
+                  <span className='time-image' />
+                  <p>{d.quantity} {d.quantity > 1 ? 'pills' : 'pill'} {d.frequency}x per days</p>
+                </div>
+                <div className='image-content-container'>
+                  <span className='calendar-image' />
+                  <p>For {d.traitment_duration} days</p>
+                </div>
+                <div className='image-content-container'>
+                  <span className='info-image' />
+                  <p>{d.additional_information}</p>
+                </div>
+              </div>
             </div>
-            <div className='image-content-container'>
-              <span className='calendar-image' />
-              <p>For {d.traitment_duration} days</p>
-            </div>
-            <div className='image-content-container'>
-              <span className='info-image' />
-              <p>{d.additional_information}</p>
-            </div>
-          </div>
-        </div>
       )
     })}
 
