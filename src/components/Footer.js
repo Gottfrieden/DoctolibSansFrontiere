3b8614b680a801ccd291adc78a3de76ffcd26554
ikramb-ctrl <b_zakia@homail.com>
2020-06-25 21:17:54 +0200

    ajout du footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 const Footer = () => {
-  return (
-    <footer />
-  );
+return (
+<footer>
+
+<div className='footer-style'>
+<p>© 2020 Copyright : By Wilders in partnership with Doctolib 💊 </p>
+
+</div>
+</footer>
+);
 };
 
 export default Footer;
